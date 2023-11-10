6d88cd8d7b5772acce666ac301c6ddc06b99a58d
Shuster Oleksandr <godzilasanya@gmail.com>
2023-11-10 17:53:07 +0200

    task-1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,32 @@
+import getRandomHexColor from '../helpers/randomHexColor';
 
+const start = document.querySelector('button[data-start]');
+const stop = document.querySelector('button[data-stop]');
+const body = document.querySelector('body');
+
+start.addEventListener('click', changeColor);
+stop.addEventListener('click', stopChange);
+
+stop.style.opacity = 0.6;
+stop.style.pointerEvents = 'none';
+
+let timerId = null;
+
+function changeColor() {
+  start.style.opacity = 0.6;
+  start.style.pointerEvents = 'none';
+  stop.style.opacity = 1;
+  stop.style.pointerEvents = 'auto';
+  body.style.backgroundColor = getRandomHexColor();
+  timerId = setInterval(() => {
+    body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+}
+
+function stopChange() {
+  stop.style.opacity = 0.6;
+  stop.style.pointerEvents = 'none';
+  start.style.pointerEvents = 'auto';
+  start.style.opacity = 1;
+  clearInterval(timerId);
+}
