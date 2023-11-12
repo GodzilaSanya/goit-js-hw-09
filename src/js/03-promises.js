289a87275faa09e376e7c4838e29d41107b4eaea
Shuster Oleksandr <godzilasanya@gmail.com>
2023-11-12 16:21:43 +0200

    task-3 done

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,35 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+import { Notify } from 'notiflix';
+
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', handlerInput);
+
+function handlerInput(e) {
+  e.preventDefault();
+  const delay = Number(e.target.elements.delay.value);
+  const step = Number(e.target.elements.step.value);
+  const amount = Number(e.target.elements.amount.value);
+  for (let i = 1, delayStep = delay; i <= amount; i++) {
+    createPromise(i, delayStep)
+      .then(({ position, delay }) => {
+        Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+    delayStep += step;
   }
+}
+
+function createPromise(position, delay) {
+  return new Promise((res, rej) => {
+    setTimeout(() => {
+      const shouldResolve = Math.random() > 0.3;
+      if (shouldResolve) {
+        res({ position: position, delay: delay });
+      } else {
+        rej({ position: position, delay: delay });
+      }
+    }, delay);
+  });
 }
